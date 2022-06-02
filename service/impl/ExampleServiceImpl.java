624d5b2caaa9e1e33a1010e58ed52d48c07fd681
Jesus Suarez <37440029+jsuarez1994@users.noreply.github.com>
2022-06-02 11:33:35 +0200

    Update ExampleServiceImpl.java

--- service/impl/ExampleServiceImpl.java
+++ service/impl/ExampleServiceImpl.java
@@ -1,12 +1,12 @@
-package es.santander.ucrapl.mcs.service.impl;
+package service.impl;
 
 import org.springframework.stereotype.Service;
 
-import es.santander.ucrapl.mcs.documents.ExampleDocument;
-import es.santander.ucrapl.mcs.dto.ExampleDTO;
-import es.santander.ucrapl.mcs.map.ExampleMapper;
-import es.santander.ucrapl.mcs.repository.ExampleRepository;
-import es.santander.ucrapl.mcs.service.ExampleService;
+import documents.ExampleDocument;
+import dto.ExampleDTO;
+import map.ExampleMapper;
+import repository.ExampleRepository;
+import service.ExampleService;
 
 /**
  * ExampleServiceImpl
